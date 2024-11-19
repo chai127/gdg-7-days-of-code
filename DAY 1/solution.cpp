bc475b45f0f4d78934c3f45b9298488652f6a13c
chai127 <chaitraraje27@gmail.com>
2024-11-19 18:38:57 +0530

    Create solution.cpp

---
+++ DAY 1/solution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string ip;
+    cout << "Say a word!\n";
+    cin >>ip;
+    cout << ip << " " <<ip;
+  return 0;
+}
